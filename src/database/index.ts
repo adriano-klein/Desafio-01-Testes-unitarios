ccf8d3e75fc2d392e7e6e149cf082265ea8be826
adriano25klein@gmail.com <you@example.com>
2021-09-03 08:25:55 -0300

    Teste de integração do CreateStatementController

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,15 @@
-import { createConnection, getConnectionOptions, Connection } from 'typeorm';
+import { createConnection, getConnectionOptions, Connection } from "typeorm";
 
-export default async(host = "db_desafio_finapi"): Promise<Connection> => {
+export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-  console.log(defaultOptions.database)
+
   return createConnection(
-
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
-      process.env.NODE_ENV === 'test'
-      ? 'db_desafio_finapi_test'
-      : defaultOptions.database
-      })
-      )
-}
-
-
+        process.env.NODE_ENV === "test"
+          ? "db_desafio_finapi_test"
+          : defaultOptions.database,
+    })
+  );
+};
---
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -0,0 +1,129 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+
+import { app } from "../../../../app";
+import createConnection from "../../../../database";
+
+let connection: Connection;
+
+describe("Create Statement Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should not be able to create a withdraw without funds", async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "Adriano Klein",
+      email: "email@email.com",
+      password: "123456",
+    });
+
+    const response = await request(app).post("/api/v1/sessions").send({
+      email: "email@email.com",
+      password: "123456",
+    });
+    const { token } = response.body;
+
+    const withdraw = await request(app)
+      .post("/api/v1/statements/withdraw")
+      .send({
+        amount: 100,
+        description: "This is just a test",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+    expect(withdraw.status).toBe(400);
+  });
+
+  it("Should not be able to create a statement by an unauthorized user", async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "Adriano Klein",
+      email: "email@email.com",
+      password: "123456",
+    });
+
+    await request(app).post("/api/v1/sessions").send({
+      email: "email@email.com",
+      password: "123456",
+    });
+
+    const token = "InvalidToken";
+    const deposit = await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 120.0,
+        description: "New deposit",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+    expect(deposit.status).toBe(401);
+  });
+
+  it("Should be able to create a withdraw", async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "Adriano Klein",
+      email: "email@email.com.br",
+      password: "123456",
+    });
+
+    const response = await request(app).post("/api/v1/sessions").send({
+      email: "email@email.com.br",
+      password: "123456",
+    });
+
+    const { token } = response.body;
+
+    await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 750.0,
+        description: "just a single deposit",
+      })
+      .set({ Authorization: `Bearer: ${token}` });
+
+    const withdraw = await request(app)
+      .post("/api/v1/statements/withdraw")
+      .send({
+        amount: 150.0,
+        description: "Just a single withdraw",
+      })
+      .set({
+        Authorization: `Bearer: ${token}`,
+      });
+    expect(withdraw.status).toBe(201);
+  });
+
+  it("Should be able to create a deposit", async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "Adriano Klein",
+      email: "email@email.com",
+      password: "123456",
+    });
+
+    const response = await request(app).post("/api/v1/sessions").send({
+      email: "email@email.com",
+      password: "123456",
+    });
+
+    const { token } = response.body;
+
+    const deposit = await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 150.0,
+        description: "Making a new deposit",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+    expect(deposit.status).toBe(201);
+  });
+});
